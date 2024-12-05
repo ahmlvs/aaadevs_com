27a64c8d437efec0894826bf18385554f1148d67
ahmlvs <andrejs.hmelovs@gmail.com>
2024-12-05 17:52:40 +0400

    host for prod

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ if __name__ == "__main__":
     PRODUCTION = os.getenv("PRODUCTION", "False") == "True"
 
     if PRODUCTION:
-        uvicorn.run("main:app", host="127.0.0.1", port=8000)
+        uvicorn.run("main:app", host="0.0.0.0", port=8000)
     else:
         uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
